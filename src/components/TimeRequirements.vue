3cc7d68517559aad5534adbbbac678dcc70668f0
Jacob Alberty <jacob.alberty@gmail.com>
2022-11-17 06:08:49 -0600

    Move loading of json to App.vue and parse into a defined type

--- src/components/TimeRequirements.vue
+++ src/components/TimeRequirements.vue
@@ -1,17 +1,17 @@
 <template>
   <v-container>
     <table>
-      <tr>LPOC: <v-select name="LPOC" id="lpoc" :items="lpoc" item-value="Miles" item-text="ID"
+      <tr>LPOC: <v-select name="LPOC" id="lpoc" :items="lpoc" item-value="value" item-text="label"
           v-on:change="changeLPOC"></v-select>
       </tr>
       <tr>LPOC MM: <input type="number" step="0.01" id="lpocmm" name="lpocmm" v-model="lpocmm"></tr>
-      <tr>Destination: <v-select name="Destination" :items="destination" item-value="Mile Marker"
-          item-text="Destination" v-on:change="changeDest"></v-select>
+      <tr>Destination: <v-select name="Destination" :items="destination" item-value="value" item-text="label"
+          v-on:change="changeDest"></v-select>
       </tr>
       <tr>Destination MM: <input type="number" step="0.01" id="destmm" name="destmm" v-model="destmm"></tr>
       <tr>Time Submitted: <input type="datetime-local" id="subtime" name="subtime" v-model="subtime"></tr>
-      <tr>Minimum Time: {{minimumTime}}</tr>
-      <tr>Cleared Time: {{clearedTime}}</tr>
+      <tr>Minimum Time: {{ minimumTime }}</tr>
+      <tr>Cleared Time: {{ clearedTime }}</tr>
       <!--
       <tr>Minimum Time: <input readonly="readonly" type="datetime-local" id="mintime" name="mintime" v-model="minimumTime"></tr>
       <tr>Cleared Time: <input readonly="readonly" type="datetime-local" id="clrdtime" name="clrdtime" v-model="clearedTime"></tr>
@@ -21,9 +21,6 @@
 </template>
 
 <script>
-import lpoc from '../../json/lpoc.json';
-import destination from '../../json/destination.json';
-
 Date.prototype.addHours = function(h) {
   this.setTime(this.getTime() + (h*60*60*1000));
   return this;
@@ -31,19 +28,27 @@ Date.prototype.addHours = function(h) {
 
 export default {
   name: 'TimeRequirements',
+  props: {
+    lpoc: {
+      type: Array,
+      default: () => [],
+    },
+    destination: {
+      type: Array,
+      default: () => [],
+    },
+  },
   data: () => {
     return {
       publicPath: process.env.BASE_URL,
       subtime: new Date().toISOString().replace('Z', ''),
       destmm: 0.00,
       lpocmm: 0.00,
-      lpoc: lpoc.sort((a, b) => a.ID.localeCompare(b.ID)),
-      destination: destination.sort((a, b) => a.Destination.localeCompare(b.Destination))
     }
   },
   methods: {
     changeLPOC(lpocmm) {
-      this.lpocmm = parseFloat(lpocmm.replace(/,/g, ''));
+      this.lpocmm = lpocmm;
     },
     changeDest(destmm) {
       this.destmm = destmm;
